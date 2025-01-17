35e5e748708dbad10d3cc03eca40b8244137d282
Naimur2 <naimurrahaman248@gmail.com>
2025-01-17 23:44:36 +0600

    Update Rollup configuration to reorder plugins and ensure gzip compression is applied

--- rollup.config.js
+++ rollup.config.js
@@ -2,14 +2,13 @@ import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import url from "@rollup/plugin-url";
 import bakedEnv from "rollup-plugin-baked-env";
+import gzipPlugin from "rollup-plugin-gzip";
 import polyfillNode from "rollup-plugin-polyfill-node";
 import postcss from "rollup-plugin-postcss";
 import { importAsString } from "rollup-plugin-string-import";
 import { terser } from "rollup-plugin-terser";
-import typescript from "rollup-plugin-typescript2";
 import tsConfigPaths from "rollup-plugin-ts-paths";
-import gzipPlugin from "rollup-plugin-gzip";
-import { babel } from "@rollup/plugin-babel";
+import typescript from "rollup-plugin-typescript2";
 
 const myVariables = {
   NODE_ENV: "production",
@@ -54,11 +53,6 @@ export default {
       limit: Infinity,
     }),
     tsConfigPaths(),
-    gzipPlugin(),
-    babel({
-      babelHelpers: "bundled",
-      exclude: "node_modules/**",
-    }),
-  ],
-  external: ["react", "react-dom"],
+    gzipPlugin()
+  ]
 };
