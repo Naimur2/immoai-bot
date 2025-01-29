4abef0f0a73f5566732540c9f2ecca6c16982801
naimur rahaman <naim@getg3ms.com>
2025-01-29 19:58:34 +0600

    Refactor ChatbotPage component: improve formatting and add bot image to message bubbles

--- src/content/ChatPage.tsx
+++ src/content/ChatPage.tsx
@@ -3,51 +3,61 @@ import ChatBot from "../components/ChatBot";
 import useChatSettings from "../context/useChatSettings";
 
 const ChatbotPage = () => {
-  const { isLoading, data, sendMessage, messages,isChatLoading } = useChatSettings();
+    const { isLoading, data, sendMessage, messages, isChatLoading } =
+        useChatSettings();
 
-  if (isLoading || !data) {
-    return null;
-  }
+    if (isLoading || !data) {
+        return null;
+    }
 
-  return (
-    <>
-      <ChatBot.Content>
-        {messages.length === 0 ? (
-          <ChatBot.Bubble
-            type="bot"
-            text={
-              data?.assistant_styles?.text_to_appear ||
-              "Hello, how can I help you today?"
-            }
-            color={data?.assistant_styles?.bot_bubble_text}
-            backgroundColor={data?.assistant_styles?.bot_bubble_color}
-            image={data?.assistant_styles?.chat_widget_image}
-            imageBackgroundColor={data?.assistant_styles?.bot_avatar_color}
-          />
-        ) : null}
+    return (
+        <>
+            <ChatBot.Content>
+                {messages.length === 0 ? (
+                    <ChatBot.Bubble
+                        type="bot"
+                        text={
+                            data?.assistant_styles?.text_to_appear ||
+                            "Hello, how can I help you today?"
+                        }
+                        color={data?.assistant_styles?.bot_bubble_text}
+                        backgroundColor={
+                            data?.assistant_styles?.bot_bubble_color
+                        }
+                        image={data?.assistant_styles?.chat_widget_image}
+                        imageBackgroundColor={
+                            data?.assistant_styles?.bot_avatar_color
+                        }
+                    />
+                ) : null}
 
-        {messages.map((message, index) => (
-          <ChatBot.Bubble
-            key={index}
-            type={message.type}
-            text={message.message}
-            color={
-              message.type === "user"
-                ? data?.assistant_styles?.user_bubble_text
-                : data?.assistant_styles?.bot_bubble_text
-            }
-            backgroundColor={
-              message.type === "user"
-                ? data?.assistant_styles?.bubble_color_user
-                : data?.assistant_styles?.bot_bubble_color
-            }
-          />
-        ))}
-        {isChatLoading && <span className="chat-loader"></span>}
-      </ChatBot.Content>
-      <ChatBot.ToolBar onSend={sendMessage} />
-    </>
-  );
+                {messages.map((message, index) => (
+                    <ChatBot.Bubble
+                        key={index}
+                        type={message.type}
+                        text={message.message}
+                        color={
+                            message.type === "user"
+                                ? data?.assistant_styles?.user_bubble_text
+                                : data?.assistant_styles?.bot_bubble_text
+                        }
+                        backgroundColor={
+                            message.type === "user"
+                                ? data?.assistant_styles?.bubble_color_user
+                                : data?.assistant_styles?.bot_bubble_color
+                        }
+                        image={
+                            message.type === "bot"
+                                ? data?.assistant_general?.bot_image
+                                : undefined
+                        }
+                    />
+                ))}
+                {isChatLoading && <span className="chat-loader"></span>}
+            </ChatBot.Content>
+            <ChatBot.ToolBar onSend={sendMessage} />
+        </>
+    );
 };
 
 export default ChatbotPage;
